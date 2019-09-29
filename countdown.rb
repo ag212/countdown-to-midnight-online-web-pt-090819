209a73b7255d16b097ed7a18799ce627175155fa
Abraham Galva <ag212@icloud.com>
2019-09-29 02:03:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
-
 def countdown
+number = 10
+while number < 0 
+  puts "#{number} SECOND(S)!"
+  counter -= 1
+return "HAPPY NEW YEAR!"
+end
+end
+
+def countdown_with_sleep
+  sleep (5)
 end
