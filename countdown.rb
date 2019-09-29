de84a45e616907b5076f616560b93bf21bf8e5ba
Abraham Galva <ag212@icloud.com>
2019-09-29 02:07:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,10 @@
-def countdown
-number = 10
-while number < 0 
-  puts "#{number} SECOND(S)!"
-  counter -= 1
-return "HAPPY NEW YEAR!"
+def countdown(number)
+  number = 10 
+while number > 0 
+puts "#{number} SECOND(S)!"
+number -= 1
+end 
+until number == 0
+puts "HAPPY NEW YEAR"
 end
-end
-
-def countdown_with_sleep
-  sleep (5)
 end
